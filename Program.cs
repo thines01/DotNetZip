4cb6d4d1648a1237bd6f779a21646565b594dab0
thines01 <thines01@yahoo.com>
2023-07-07 20:09:10 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DotNetZip
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+      }
+   }
+}
